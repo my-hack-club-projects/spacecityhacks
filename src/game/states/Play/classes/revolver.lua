04947d33b4207bf346ea9a0a4eb5b04f267917a4
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-20 17:45:08 +0200

    added isCocked function

--- src/game/states/Play/classes/revolver.lua
+++ src/game/states/Play/classes/revolver.lua
@@ -5,6 +5,7 @@ local Revolver = oo.class()
 function Revolver:init()
     self.bullets = 6
     self.shots = 0
+    self.cocked = false
 
     self.position = math.random(1, self.bullets)
 
@@ -19,12 +20,18 @@ function Revolver:cock()
     if self.position > self.bullets then
         self.position = 1
     end
+    self.cocked = true
 end
 
 function Revolver:shoot()
+    self.cocked = false
     self.shots = self.shots + 1
     local shot = self.chamber[self.position]
     return shot
+end
+
+function Revolver:isCocked()
+    return self.cocked
 end
 
 return Revolver
