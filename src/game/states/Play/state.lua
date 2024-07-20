caa2d04ba039ad4e698d137ebd6216a1db7a566e
Vítek Adam <stuckinvim@protonmail.com>
2024-07-20 17:58:10 +0200

    Added round logic

--- src/game/states/Play/state.lua
+++ src/game/states/Play/state.lua
@@ -1,5 +1,6 @@
 local oo = require 'libs.oo'
 local State = require 'classes.state'
+local Revolver = require('game.states.Play.classes.revolver')
 
 local PlayState = oo.class(State)
 
@@ -7,10 +8,40 @@ function PlayState:init(game)
     State.init(self, game)
 
     self.name = "PlayState"
+
+    self.revolver = Revolver()
+    self.roundInterval = 2
 end
 
 function PlayState:enter(prevState)
     State.enter(self, prevState)
+
+    self.canShoot = true
+
+    self.mousePressedListener = self.game.signals.mousepressed:connect(function()
+        if not self.canShoot then
+            return
+        end
+
+        if self.revolver:isCocked() then
+            local die = self.revolver:shoot()
+            if die then
+                print("player dies")
+                return
+            end
+            -- enemies' turn
+            self.game:defer(self.roundInterval, function()
+                self.revolver:cock()
+                print("Enemy cocked gun")
+                local die = self.revolver:shoot()
+                if die then
+                    print("enemy dies")
+                end
+            end)
+        else
+            self.revolver:cock()
+        end
+    end)
 end
 
 function PlayState:exit()
